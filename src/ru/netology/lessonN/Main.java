82e4c066ba827810466b6bba637a38260dc536a1
MalinovskaNA <delenmir@yandex.ru>
2022-03-29 15:52:06 +0300

    home work stream api 1

---
+++ src/ru/netology/lessonN/Main.java
@@ -0,0 +1,22 @@
+package ru.netology.lessonN;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> rezult = new ArrayList<>();
+
+        for (int x : intList) {
+            if (x > 0 && x % 2 == 0) {
+                rezult.add(x);
+            }
+        }
+        rezult.sort(Comparator.naturalOrder());
+        System.out.println(rezult);
+    }
+}
